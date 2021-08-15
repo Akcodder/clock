b438c1155b9a75572e54906613423efe41ed90f6
Afran khan <87820120+afrankhan@users.noreply.github.com>
2021-08-15 09:39:08 +0530

    Update clock.c

--- clock.c
+++ clock.c
@@ -3,13 +3,13 @@
 int main()
 {
    int a;
-   printf("Enter a Number which is Even if it is not even so cannot see time and Date\n ");
+   printf("Enter a Number which is Even if it is not even so cannot able to  see Time and Date\n ");
    scanf("%d",&a);
    if(a%2==0){
     printf("Current Date :%s\n", __DATE__ );
     printf("Current Time :%s\n", __TIME__ );
    }
    else{
-   	printf("your input is odd");
+   	printf("Your input is odd");
    }
 }
